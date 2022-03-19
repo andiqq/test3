509270ef24aba8d4fca4b3d06025817c263a54c2
Dr. Andreas Plagens <andreas@plagens.eu>
2022-03-19 16:18:43 +0100

    for tests

--- test3Tests/test3Tests.swift
+++ test3Tests/test3Tests.swift
@@ -10,21 +10,21 @@ import XCTest
 
 class test3Tests: XCTestCase {
 
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-    }
+//    override func setUpWithError() throws {
+//        // Put setup code here. This method is called before the invocation of each test method in the class.
+//    }
+//
+//    override func tearDownWithError() throws {
+//        // Put teardown code here. This method is called after the invocation of each test method in the class.
+//    }
 
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-    }
-
-    func testExample() throws {
+    // func testExample() throws {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct results.
         // Any test you write for XCTest can be annotated as throws and async.
         // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
         // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
-    }
+ //   }
     
     func testMethod() {
         let number1 = 3
@@ -35,11 +35,10 @@ class test3Tests: XCTestCase {
         XCTAssertEqual(7, result)
     }
 
-    func testPerformanceExample() throws {
+//    func testPerformanceExample() throws {
         // This is an example of a performance test case.
-        self.measure {
+ //       self.measure {
             // Put the code you want to measure the time of here.
-        }
-    }
+ //  }
 
 }
