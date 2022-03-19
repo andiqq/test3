1721af1a8b4ed59fccb30361831ec90015dd748f
Dr. Andreas Plagens <andreas@plagens.eu>
2022-03-19 15:22:35 +0100

    Initial Commit

---
+++ test3/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  test3
+//
+//  Created by Dr. Andreas Plagens on 19.03.22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
